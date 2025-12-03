ce38c6193addb6134b6d0ea1d95fbe0992952367
crp3 <caiorochapstrlrd@gmail.com>
2025-12-03 16:28:50 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@
 This is a SHELL challenge
 """
 
+import os
+import stat
 import sys
 from typing import List, Optional, Tuple
 
@@ -84,13 +86,43 @@ def handle_command(command: str, args: List[str]):
         case "echo":
             sys.stdout.write(" ".join(args) + "\n")
         case "type":
-            local_cmd = args[0]
-            if local_cmd not in valid_commands:
+            local = args[0]
+            if local in valid_commands:
+                sys.stdout.write(f"{local} is a shell builtin\n")
+                return
+
+            found, path = browse_path(local)
+            if found:
+                sys.stdout.write(f"{args[0]} is {path}\n")
+            else:
                 raise ArgumentNotFoundException
-            sys.stdout.write(f"{local_cmd} is a shell builtin\n")
 
         case _:
             raise CommandNotFoundException
+
+
+def browse_path(local: str) -> Tuple[bool, str]:
+    """
+    Browse a filename in PATH and return only if executable
+    """
+    path_str = os.environ["PATH"]
+    path_str_split = path_str.split(":")
+    for directory_path in path_str_split:
+        try:
+            contents = os.listdir(directory_path)
+            for content in contents:
+                if content == local:
+                    file_path = os.path.join(directory_path, content)
+                    st = os.stat(file_path)
+                    is_executable = st.st_mode & (
+                        stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH
+                    )
+                    if is_executable:
+                        return True, file_path
+        except FileNotFoundError:
+            continue
+
+    return False, ""
 
 
 def parse_command(input_str: Optional[str]) -> Tuple[str, List[str]]:
